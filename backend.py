a060f7d8761ef5d4f9774c192f07ff4c5a03e562
Charan C <72061061+ccharan@users.noreply.github.com>
2023-10-17 14:15:41 +0530

    Delete backend.py

--- backend.py
+++
@@ -1,6 +0,0 @@
-from deep_translator import GoogleTranslator
-
-
-def translate_text(source, target, text):
-    translated = GoogleTranslator(source=source, target=target).translate_batch([text])
-    return translated
